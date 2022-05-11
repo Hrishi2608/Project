28e9c775a85b9409e2dea0c0507d759ddca1672c
Hrishi2608 <98205148+Hrishi2608@users.noreply.github.com>
2022-05-11 08:38:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            listener.adjust_for_ambient_noise(source)
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice, language='en-US')
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'angry' in command:
+        talk('sorry, I have a headache')
+    elif 'are you single' in command:
+        talk('No I am not')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    elif 'tired' in command:
+        talk('It is time to go to sleep')
+    elif 'birthday' in command:
+        talk('Remainder to check your friends list')
+    else:
+        talk('Please say the command again.')
+
+
+while True:
+    run_alexa()
